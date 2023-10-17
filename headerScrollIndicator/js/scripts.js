ead0d40d3707c53d5080403413941bd2cbf8d152
ungveloper <106213837+ungveloper@users.noreply.github.com>
2023-10-17 15:49:52 +0900

    Fix: header scroll indicator width error

--- headerScrollIndicator/js/scripts.js
+++ headerScrollIndicator/js/scripts.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const header = document.querySelector("#header");
+  const header = document.querySelector("header");
 
   if (header) {
     const headerScrollIndicator = document.createElement("div");
@@ -10,7 +10,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const updateProgress = () => {
       const totalHeight = document.body.scrollHeight - window.innerHeight;
       const scrolled = window.scrollY;
-      const progressPercentage = (scrolled / totalHeight) * 100;
+      let progressPercentage = (scrolled / totalHeight) * 100;
+      if (progressPercentage > 99.93) {
+        progressPercentage = 100;
+      }
 
       headerScrollIndicator.style.width = `${progressPercentage}%`;
     };
